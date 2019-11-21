9049cef2ba488bebb60089b340e20a40fe878c87
Cuatepotzo Martinez Sebastian Alberto <XME4999@cb.bbvabancomer.com.mx>
2019-11-21 17:47:53 -0600

    arquetipo para Junit test agregada

--- src/test/java/mx/gnc/as/gutsncode/GutsncodeApplicationTests.java
+++ src/test/java/mx/gnc/as/gutsncode/GutsncodeApplicationTests.java
@@ -1,16 +1,51 @@
 package mx.gnc.as.gutsncode;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class GutsncodeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@BeforeAll
+    static void setup(){
+        System.out.println("@BeforeAll executed");
+    }
+     
+    @BeforeEach
+    void setupThis(){
+        System.out.println("@BeforeEach executed");
+    }
+     
+    @Test
+    void testCalcOne() 
+    {
+        System.out.println("======TEST ONE EXECUTED=======");
+//        Assertions.assertEquals( 4 , 4);
+    }
+     
+//    @Test
+//    void testCalcTwo() 
+//    {
+//        System.out.println("======TEST TWO EXECUTED=======");
+//        Assertions.assertEquals( 6 , 6);
+//    }
+     
+    @AfterEach
+    void tearThis(){
+        System.out.println("@AfterEach executed");
+    }
+     
+    @AfterAll
+    static void tear(){
+        System.out.println("@AfterAll executed");
+    }
 
 }
