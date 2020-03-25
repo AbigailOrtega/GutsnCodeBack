64e50c19e44c20b24d5bbfa627e555a85fd7768a
SebCuate <sstsacm@gmail.com>
2020-03-25 01:01:17 -0600

    Token authentication

--- src/main/java/mx/gnc/as/gutsncode/config/security/WebSecurity.java
+++ src/main/java/mx/gnc/as/gutsncode/config/security/WebSecurity.java
@@ -61,7 +61,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security",
 						"/swagger-ui.html", "/webjars/**")
 				.permitAll() // Swagger
-				//.antMatchers("/managment/getTopic/*","/managment/*").permitAll() // User services don't need
+				//.antMatchers("/managment/getTopic/*","/managment/*").permitAll() // User services don't need   
 				.antMatchers("/gncu/*","/h2-console/*").permitAll() // User services don't need authorization
 				.antMatchers(HttpMethod.POST, "/login").permitAll() // Login doesn't need authorization
 				.anyRequest().authenticated(); // All other pages are securized
