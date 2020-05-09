551e892d83398268d34256123889d8e5be919567
SebCuate <sstsacm@gmail.com>
2020-05-09 15:44:23 -0500

    URL Front changed

--- src/main/java/mx/gnc/as/gutsncode/controller/CrossOriginURL.java
+++ src/main/java/mx/gnc/as/gutsncode/controller/CrossOriginURL.java
@@ -2,7 +2,7 @@ package mx.gnc.as.gutsncode.controller;
 
 public class CrossOriginURL {
 
-//	protected static final String URL_FRONT = "https://guts-n-code-test.herokuapp.com";
-	protected static final String URL_FRONT = "http://localhost:4200";
+	protected static final String URL_FRONT = "https://guts-n-code-test.herokuapp.com";
+//	protected static final String URL_FRONT = "http://localhost:4200";
 	
 }
